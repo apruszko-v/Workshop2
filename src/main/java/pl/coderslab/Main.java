12be120324619760ba2fd48c9b95bc05e5ab41f6
apruszko-v <apruszko@visa.com>
2025-07-04 15:12:37 +0200

    main method

--- src/main/java/pl/coderslab/Main.java
+++ src/main/java/pl/coderslab/Main.java
@@ -1,7 +1,34 @@
 package pl.coderslab;
 
+import pl.coderslab.util.DbUtil;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
     }
 }
+
+
+
+
+
+
+
+//        try (Connection conn = DbUtil.getConnection();
+//             Statement stat = conn.createStatement()) {
+//            ResultSet resultSet = stat.executeQuery("select id, email from users");
+//
+//            String[] columns = {"id", "email"};
+//
+//            while (resultSet.next()) {
+//                for (String column : columns) {
+//                    System.out.print(resultSet.getString(column) + " ");
+//                }
+//                System.out.println();
+//            }
+//        }
